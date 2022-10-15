510b01de9ac855891540781544ac74f2d0549127
Bill <Bill@TheWallisFamily.co.uk>
2022-10-15 09:19:01 +0100

    Refactored the profiling script to handle multiple scripts dynamically

--- query-profiling/query_timer.py
+++ query-profiling/query_timer.py
@@ -1,6 +1,13 @@
+import datetime
 import sqlite3
-from timeit import timeit
+from dataclasses import dataclass
 from functools import partial
+from timeit import timeit
+from os import listdir
+from os.path import isfile, join
+from typing import Generator, Callable
+
+from tqdm import trange
 
 
 class DbConnection:
@@ -14,25 +21,56 @@ class DbConnection:
             return self.connection.execute(f.read())
 
 
-def run_query_1(conn: DbConnection):
-    conn.execute_file('queries/query-1.sql')
+def get_query_filepaths(directory: str = 'queries') -> Generator:
+    """Generator of query filepaths."""
+    # sourcery skip: instance-method-first-arg-name
+    for path in listdir(directory):
+        full_path = join(directory, path)
+        if isfile(full_path):
+            yield full_path
 
 
-def run_query_2(conn: DbConnection):
-    conn.execute_file('queries/query-2.sql')
+@dataclass
+class Runner:
+    runner: Callable
+    filepath: str
+    repeat: int = 0
+    total_time: float = 0.0
+
+    def __call__(self):
+        self.runner()
+
+    @property
+    def average_time(self):
+        return self.total_time / self.repeat
+
+    @property
+    def file(self):
+        return self.filepath[1 + self.filepath.rfind('\\'):]
 
 
 def time_queries(repeat: int):
     db_conn = DbConnection('../db-creation/customer-db/customer.db')
+    runners: list[Runner] = [
+        Runner(
+            runner=partial(db_conn.execute_file, filepath=query_filepath),
+            filepath=query_filepath
+        ) for query_filepath in get_query_filepaths()
+    ]
+    print('Start time:', datetime.datetime.now())
 
     # Set the 'temp' tables
-    run_query_1(conn=db_conn)
-    run_query_2(conn=db_conn)
+    for runner in runners:
+        runner()
 
-    # Query stats
-    query_1_avg_time = timeit(partial(run_query_1, db_conn), number=repeat) / repeat
-    query_2_avg_time = timeit(partial(run_query_2, db_conn), number=repeat) / repeat
-    total_avg_time = query_1_avg_time + query_2_avg_time
+    # We want to keep running them 'side-by-side' to account for busy periods in the DB
+    for _ in trange(repeat):
+        for runner in runners:
+            runner.repeat += 1
+            runner.total_time += timeit(runner, number=1)
 
-    print(f'Query 1: {query_1_avg_time:.8f} ({query_1_avg_time/total_avg_time:.1%})', )
-    print(f'Query 2: {query_2_avg_time:.8f} ({query_2_avg_time/total_avg_time:.1%})', )
+    total_avg_time = sum(runner.average_time for runner in runners)
+    for runner in runners:
+        print(f'{runner.file}: {runner.average_time:.8f} ({runner.average_time/total_avg_time:.1%})', )
+
+    print('End time:', datetime.datetime.now())
